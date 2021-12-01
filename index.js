de1e5ecadbf806b1560edbbb9cab5bc3a104c285
MD Sanju <mdsanju387@gmail.com>
2021-12-01 17:14:17 +0600

    portfolio server added

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5gvym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('portfolio_website');
+        const projectsCollection = database.collection("projects");
+        const usersCollection = database.collection("users");
+
+        // POST API
+        app.post('/projects', async (req, res) => {
+            const project = req.body;
+            const result = await projectsCollection.insertOne(project);
+            res.json(result);
+        })
+
+        // GET API
+        app.get('/projects', async (req, res) => {
+            const cursor = projectsCollection.find({});
+            const projects = await cursor.toArray();
+            res.send(projects);
+        })
+
+    // UPDATE API
+    app.put("/users", async (req, res) => {
+        const user = req.body;
+        const filter = { email: user.email };
+        const options = { upsert: true };
+        const updateDoc = { $set: user };
+        const result = await usersCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.json(result);
+      });
+
+    // UPDATE API (make an admin)
+    app.put('/users/admin', async (req, res) => {
+        const user = req.body;
+        const filter = {email: user.email}
+        const updateDoc = { $set: { role: "admin" } };
+        const result = await usersCollection.updateOne(filter, updateDoc)
+        res.json(result)
+        })
+
+        // GET API (matched admin role)
+        app.get('/users/:email', async (req, res) => {
+          const email = req.params.email;
+          const query = { email: email };
+          const user = await usersCollection.findOne(query);
+          let isAdmin = false;
+          if (user?.role === 'admin') {
+            isAdmin = true;
+          }
+          res.json({ admin: isAdmin });
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Portfolio Website is Running!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
