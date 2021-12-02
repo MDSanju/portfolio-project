59b363d32aff0b17b0927f62eba66a2c3c90f02a
MD Sanju <mdsanju387@gmail.com>
2021-12-03 04:38:57 +0600

    detail page added

--- index.js
+++ index.js
@@ -1,77 +1,87 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const cors = require("cors");
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5gvym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
-    try {
-        await client.connect();
-        const database = client.db('portfolio_website');
-        const projectsCollection = database.collection("projects");
-        const usersCollection = database.collection("users");
+  try {
+    await client.connect();
+    const database = client.db("portfolio_website");
+    const projectsCollection = database.collection("projects");
+    const usersCollection = database.collection("users");
 
-        // POST API
-        app.post('/projects', async (req, res) => {
-            const project = req.body;
-            const result = await projectsCollection.insertOne(project);
-            res.json(result);
-        })
+    // POST API
+    app.post("/projects", async (req, res) => {
+      const project = req.body;
+      const result = await projectsCollection.insertOne(project);
+      res.json(result);
+    });
 
-        // GET API
-        app.get('/projects', async (req, res) => {
-            const cursor = projectsCollection.find({});
-            const projects = await cursor.toArray();
-            res.send(projects);
-        })
+    // GET API
+    app.get("/projects", async (req, res) => {
+      const cursor = projectsCollection.find({});
+      const projects = await cursor.toArray();
+      res.send(projects);
+    });
 
     // UPDATE API
     app.put("/users", async (req, res) => {
-        const user = req.body;
-        const filter = { email: user.email };
-        const options = { upsert: true };
-        const updateDoc = { $set: user };
-        const result = await usersCollection.updateOne(
-          filter,
-          updateDoc,
-          options
-        );
-        res.json(result);
-      });
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
 
     // UPDATE API (make an admin)
-    app.put('/users/admin', async (req, res) => {
-        const user = req.body;
-        const filter = {email: user.email}
-        const updateDoc = { $set: { role: "admin" } };
-        const result = await usersCollection.updateOne(filter, updateDoc)
-        res.json(result)
-        })
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
 
-        // GET API (matched admin role)
-        app.get('/users/:email', async (req, res) => {
-          const email = req.params.email;
-          const query = { email: email };
-          const user = await usersCollection.findOne(query);
-          let isAdmin = false;
-          if (user?.role === 'admin') {
-            isAdmin = true;
-          }
-          res.json({ admin: isAdmin });
-        })
-    }
-    finally {
-        // await client.close();
-    }
+    // GET API (matched admin role)
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // GET API by ID
+    app.get("/projects/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const project = await projectsCollection.findOne(query);
+      res.json(project);
+    });
+  } finally {
+    // await client.close();
+  }
 }
 
 run().catch(console.dir)
